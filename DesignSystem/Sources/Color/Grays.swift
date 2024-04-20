4dfc1da66ac358afbe95ef01699c329ca4da7f73
HongSJae <gtw030488@gmail.com>
2024-04-20 22:27:23 +0900

    ✨ :: Grays Color 수정

--- DesignSystem/Sources/Color/Grays.swift
+++ DesignSystem/Sources/Color/Grays.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
 public extension Color {
-    static let gray10 = DesignSystemAsset.gray10.swiftUIColor
-    static let gray20 = DesignSystemAsset.gray20.swiftUIColor
-    static let gray30 = DesignSystemAsset.gray30.swiftUIColor
-    static let gray40 = DesignSystemAsset.gray40.swiftUIColor
-    static let gray50 = DesignSystemAsset.gray50.swiftUIColor
-    static let gray60 = DesignSystemAsset.gray60.swiftUIColor
-    static let gray70 = DesignSystemAsset.gray70.swiftUIColor
-    static let gray80 = DesignSystemAsset.gray80.swiftUIColor
-    static let gray90 = DesignSystemAsset.gray90.swiftUIColor
-    static let gray100 = DesignSystemAsset.gray100.swiftUIColor
+    static let gray10 = DesignSystemAsset.Grays.gray10.swiftUIColor
+    static let gray20 = DesignSystemAsset.Grays.gray20.swiftUIColor
+    static let gray30 = DesignSystemAsset.Grays.gray30.swiftUIColor
+    static let gray40 = DesignSystemAsset.Grays.gray40.swiftUIColor
+    static let gray50 = DesignSystemAsset.Grays.gray50.swiftUIColor
+    static let gray60 = DesignSystemAsset.Grays.gray60.swiftUIColor
+    static let gray70 = DesignSystemAsset.Grays.gray70.swiftUIColor
+    static let gray80 = DesignSystemAsset.Grays.gray80.swiftUIColor
+    static let gray90 = DesignSystemAsset.Grays.gray90.swiftUIColor
+    static let gray100 = DesignSystemAsset.Grays.gray100.swiftUIColor
 }
